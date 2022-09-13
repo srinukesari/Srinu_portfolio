f222bad2ac17ace93bcf0c01bd53411b8b7ace85
Kesari Lakshmi Srinivas <srinukesari333@gmail.com>
2022-09-13 16:47:21 +0530

    experience added and new style added to skills set icons

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,67 +2,88 @@ import React, { Component } from 'react'
 import {Container,Row,Col} from 'react-bootstrap'
 import './All.css'
 
+
 class Skills extends Component {
   render() {
     return (
       <div id = 'skills'>
         <Container>
             <p id="pro_title">Skills</p>
-            <p id = "pro_name">Comfortable with:</p>
+            <p id = "pro_name">Worked on</p>
             <Row>
                 <Col>
-                    <center><img src="https://img.icons8.com/color/48/000000/python--v2.png"/></center>
+                    <div className='tech-logo'><center><img src="https://img.icons8.com/color/48/000000/python--v2.png"/><br/>python</center></div>
                 </Col>
                 <Col>
-                    <center><img src="https://img.icons8.com/color/48/000000/html-5.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/48/000000/html-5.png"/><br/>html</center></div>
                 </Col>
                 <Col>
-                    <center><img src="https://img.icons8.com/color/48/000000/css3.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/48/000000/css3.png"/><br/>css</center></div>
                 </Col>
                 <Col>
-                    <center><img src="https://img.icons8.com/color/48/000000/javascript--v2.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/48/000000/javascript--v2.png"/><br/>javascript</center></div>
                 </Col>
                 <Col>
-                    <center><img src="https://img.icons8.com/fluency/48/000000/mysql-logo.png"/></center>
-                </Col>
-            </Row>
-            <p id = "pro_name">Familiar With:</p>
-            <Row>
-                <Col>
-                    <center><img src="https://img.icons8.com/ios/50/000000/django.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/nolan/50/php.png"/><br/>Php</center></div>
                 </Col>
                 <Col>
-                    <center><img src="https://img.icons8.com/color/48/000000/postgreesql.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/fluency/50/000000/mysql-logo.png"/><br/>mysql</center></div>
                 </Col>
                 <Col>
-                    <center><img src="https://img.icons8.com/nolan/64/api-settings.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/officel/50/000000/react.png"/><br/>react</center></div>
                 </Col>
                 <Col>
-                    <center><img src="https://img.icons8.com/nolan/64/heroku.png"/></center>
-                </Col>
-            </Row>
-            <p id = "pro_name">Basics of:</p>
-            <Row>
-                <Col>
-                    <center><img src="https://img.icons8.com/color/48/000000/c-programming.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/48/000000/redux.png"/><br/>redux</center></div>
                 </Col>
                 <Col>
-                    <center><img src="https://img.icons8.com/color/48/000000/c-plus-plus-logo.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/48/000000/amazon-web-services.png"/><br/>aws</center></div>
                 </Col>
                 <Col>
-                    <center><img src="https://img.icons8.com/color/48/000000/java-coffee-cup-logo.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/48/000000/lambda.png"/><br/>lambda</center></div>
                 </Col>
                 <Col>
-                    <center><img src="https://img.icons8.com/officel/40/000000/react.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/nolan/50/amazon-s3.png"/><br/>s3</center></div>
                 </Col>
+                <Col>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/nolan/50/api-settings.png"/><br/>RestAPI</center></div>
+                </Col>
+                <Col>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/48/000000/lambda.png"/><br/>lambda</center></div>
+                </Col>
+                <Col>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/nolan/50/git.png"/><br/>git</center></div>
+                </Col>
+                <Col>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/48/000000/redux.png"/><br/>redux</center></div>
+                </Col>
+                <Col>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/ios/50/000000/django.png"/><br/>django</center></div>
+                </Col>
+                <Col>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/nolan/50/heroku.png"/><br/>heroku</center></div>
+                </Col>
+                <Col>
+                <div className='tech-logo'><center>
+                    <img src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/50/000000/external-netlify-a-cloud-computing-company-that-offers-hosting-and-serverless-backend-services-for-static-websites-logo-shadow-tal-revivo.png"/>netlify</center></div>
+                </Col>
+                <Col>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/50/000000/c-programming.png"/><br/>C</center></div>
+                </Col>
+                <Col>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/50/000000/c-plus-plus-logo.png"/><br/>C++</center></div>
+                </Col>
+                <Col>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/50/000000/java-coffee-cup-logo.png"/><br/>Java</center></div>
+                </Col>
+                
             </Row>
             <p id = "pro_name">IDE:</p>
             <Row>
                 <Col>
-                    <center><img src="https://img.icons8.com/color/48/000000/visual-studio-code-2019.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/48/000000/visual-studio-code-2019.png"/><br/>vs code</center></div>
                 </Col>
                 <Col>
-                    <center><img src="https://img.icons8.com/color/48/000000/pycharm.png"/></center>
+                <div className='tech-logo'><center><img src="https://img.icons8.com/color/48/000000/pycharm.png"/><br/>pycharm</center></div>
                 </Col>
                 <Col>
                 </Col>
--- src/components/Work.js
+++ src/components/Work.js
@@ -8,6 +8,19 @@ class Work extends Component {
         <Container>
             <p id="pro_title">Work Experience</p>
             <div class = "pro">
+                <p id = "pro_name">Borderfree Technologies</p>
+                <p id = "pro_con">
+                    Full Stack Developer
+                    <li>Developed 1:1 call functionality for consumer interaction with brand owners</li>
+                    <li>Implemented Permissions Engine which restricts the access to events functionalities and user settings etc., in REVO 2.0.</li>
+                    <li>Devised Authentication of Admin through OTP and Magic Link in REVO 2.0.</li>
+                    <li>Optimised Policy-Permission API calls which reduces 2.5 seconds of runtime to 700-800 ms in REVO 1.0.</li>
+                    <li>Developed JWT token Verification which provides user- access to some resources of the product in REVO 1.0.</li>
+                    <li>Integrated Advertising Engine API calls and formulated the flow in REVO 2.0.</li>
+                    <li>Resolved Production level issues in HLS Layout in REVO 1.0.</li>
+                </p>
+            </div>
+            <div class = "pro">
                 <p id = "pro_name">Virtusa </p>
                 <p id = "pro_con">
                     Associate Software Engineer
